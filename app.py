43506e0a91c55c8d7fa70707b01a7db47cf3c82c
ianuvrat <69136654+ianuvrat@users.noreply.github.com>
2021-02-01 15:00:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import dash
+import dash_auth
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.config.suppress_callback_exceptions = True
 server = app.server
+
+auth = dash_auth.BasicAuth(
+    app,
+    {'admin': 'admin',
+     'admin2': 'admin2'})
